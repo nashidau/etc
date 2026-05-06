39c9e17c520f72c49ffa910b37dc3b4373f07187
Brett Nash <bnash@apple.com>
2026-05-06 15:36:29 -0700

    Haunt.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -93,6 +93,8 @@ vim.pack.add({
   -- :MythicSceneAdjustment	Generates a Scene Adjustment
   'https://github.com/Django0033/mythic.nvim',
 
+  -- Haunt - show annotations on code lines; stored in git?
+  'https://github.com/TheNoeTrevino/haunt.nvim',
 })
 
 require('mini.basics').setup()
@@ -405,6 +407,89 @@ require('xcodebuild').setup({
 --vim.api.nvim_create_user_command('CompileCommands', 
 --	'!xcodebuild | xcpretty --report json-compilation-database --output compile_commands.json');
 
+
+
+require('haunt').opts = {
+  opts = {
+    sign = "󱙝",
+    sign_hl = "DiagnosticInfo",
+    virt_text_hl = "HauntAnnotation", -- links to DiagnosticVirtualTextHint
+    annotation_prefix = " 󰆉 ",
+    annotation_suffix = "",
+    line_hl = nil,
+    virt_text_pos = "eol",
+    data_dir = nil,
+    per_branch_bookmarks = true,
+    picker = "auto", -- "auto", "snacks", "telescope", or "fzf"
+    picker_keys = { -- picker agnostic, we got you covered
+      delete = { key = "d", mode = { "n" } },
+      edit_annotation = { key = "a", mode = { "n" } },
+    },
+  },
+}
+
+-- Haunt
+do
+  -- recommended keymaps, with a helpful prefix alias
+    local haunt = require("haunt.api")
+    local haunt_picker = require("haunt.picker")
+    local map = vim.keymap.set
+    local prefix = "<leader>h"
+
+    -- annotations
+    map("n", prefix .. "a", function()
+      haunt.annotate()
+    end, { desc = "Annotate" })
+
+    map("n", prefix .. "t", function()
+      haunt.toggle_annotation()
+    end, { desc = "Toggle annotation" })
+
+    map("n", prefix .. "T", function()
+      haunt.toggle_all_lines()
+    end, { desc = "Toggle all annotations" })
+
+    map("n", prefix .. "d", function()
+      haunt.delete()
+    end, { desc = "Delete bookmark" })
+
+    map("n", prefix .. "C", function()
+      haunt.clear_all()
+    end, { desc = "Delete all bookmarks" })
+
+    -- move
+    map("n", prefix .. "p", function()
+      haunt.prev()
+    end, { desc = "Previous bookmark" })
+
+    map("n", prefix .. "n", function()
+      haunt.next()
+    end, { desc = "Next bookmark" })
+
+    -- picker
+    map("n", prefix .. "l", function()
+      haunt_picker.show()
+    end, { desc = "Show Picker" })
+
+    -- quickfix 
+    map("n", prefix .. "q", function()
+       haunt.to_quickfix()
+    end, { desc = "Send Hauntings to QF Lix (buffer)" })
+
+    map("n", prefix .. "Q", function()
+      haunt.to_quickfix({ current_buffer = true })
+    end, { desc = "Send Hauntings to QF Lix (all)" })
+
+    -- yank
+    map("n", prefix .. "y", function()
+      haunt.yank_locations({current_buffer = true})
+    end, { desc = "Send Hauntings to Clipboard (buffer)" })
+
+    map("n", prefix .. "Y", function()
+      haunt.yank_locations()
+    end, { desc = "Send Hauntings to Clipboard (all)" })
+
+end
 
 --set guifont=Fantasque\ Sans\ Mono:h14
 
